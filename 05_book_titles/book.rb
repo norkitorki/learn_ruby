5b1300295f1c4e12c1e95acd765e3564019320a3
norkitorki <norki@tuta.io>
2019-09-21 09:29:53 +0200

    removes unnecessary regexp boundaries in the book_titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -3,7 +3,7 @@ class Book
 
     def title
         @title.split.map.with_index do |word,index|
-            (word[/(\ba\b|\ban\b|\bthe\b|\band\b|\bof\b|\bin\b)/i].nil? or index == 0) ? word.capitalize : word
+            (word[/\b(a|an|the|and|of|in)\b/i].nil? or index == 0) ? word.capitalize : word
         end.join(" ")
     end
 end
